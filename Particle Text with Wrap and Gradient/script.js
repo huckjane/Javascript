bc06a42032c3379cc8e4dc2e4ce4ab89a8d82b0c
huckjane <huckjane@users.noreply.github.com>
2023-04-21 21:16:11 -0400

    thank you page test

--- Particle Text with Wrap and Gradient/script.js
+++ Particle Text with Wrap and Gradient/script.js
@@ -1,19 +1,48 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvasThankYou');
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d', {
+      willReadFrequently: true
+    });
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    console.log(ctx);
     
    class Particle {
-      constructor(){
-
+      constructor(effect, x, y, color){
+         this.effect = effect;
+         this.x = Math.random() * this.effect.canvasWidth;
+         this.y = this.effect.canvasHeight;
+         this.color = color;
+         this.originX = x;
+         this.originY = y;
+         this.size = this.effect.gap - 1;
+         this.dx = 0;
+         this.dy = 0;
+         this.vx = 0;
+         this.vy = 0;
+         this.force = 0;
+         this.angle = 0;
+         this.distance = 0;
+         this.friction = Math.random() * 0.6 + 0.15;
+         this.ease = Math.random() * 0.1 + 0.005;
       }
       draw(){
-
+         this.effect.context.fillStyle = this.color;
+         this.effect.context.fillRect(this.x, this.y, this.size, this.size);
       }
       update(){
+         this.dx = this.effect.mouse.x - this.x;
+         this.dy = this.effect.mouse.y - this.y;
+         this.distance = this.dx * this.dx + this.dy * this.dy;
+         this.force = -this.effect.mouse.radius/this.distance;
 
+         if (this.distance < this.effect.mouse.radius){
+            this.angle = Math.atan2(this.dy, this.dx);
+            this.vx += this.force * Math.cos(this.angle);
+            this.vy += this.force * Math.sin(this.angle);
+         }
+
+         this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+         this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
       }
    }
 
@@ -27,10 +56,31 @@ window.addEventListener('load', function(){
          this.fontSize = 100;
          this.lineHeight = this.fontSize * 0.8;
          this.maxTextWidth = this.canvasWidth * 0.3;
-
+         this.textInput = document.getElementById('textInput');
+         this.verticalOffset = -160;
+         this.textInput.addEventListener('keyup', (e) => {
+            if (e.key !==' ') {
+               this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+               this.wrapText(e.target.value);
+            }
+         });
+         // Particle Text
+         this.particles = [];
+         this.gap = 5;
+         this.mouse = {
+            radius: 20000,
+            x: 0,
+            y: 0
+         }
+         window.addEventListener('mousemove', (e) => {
+            this.mouse.x = e.x;
+            this.mouse.y = e.y;
+         });
+         
       }
       wrapText(text){
-         const gradient = this.context.createLinearGradient(0, 0, canvas.width, canvas.height);
+         // Canvas Settings
+         const gradient = this.context.createLinearGradient(0, 0, this.canvasWidth, this.canvasHeight);
          gradient.addColorStop(0.4, 'green');
          gradient.addColorStop(0.5, 'gold');
          gradient.addColorStop(0.6, 'green');
@@ -40,12 +90,11 @@ window.addEventListener('load', function(){
          this.context.lineWidth = 1;
          this.context.strokeStyle = 'gold';
          this.context.font = this.fontSize + 'px Courier';
-
          // break text to new line
          let linesArray = [];
+         let words = text.split(' ');
          let lineCounter = 0;
          let line = '';
-         let words = text.split(' ');
          for (let i = 0; i < words.length; i++){
             let testLine = line + words[i] + ' ';
             if (this.context.measureText(testLine).width > this.maxTextWidth){
@@ -57,33 +106,87 @@ window.addEventListener('load', function(){
             linesArray[lineCounter] = line;
          }  
          let textHeight =  this.lineHeight * lineCounter;
-         this.textY = canvas.height/2 - textHeight/2;
+         this.textY = this.canvasHeight/2 - textHeight/2 + this.verticalOffset;
          linesArray.forEach((el, index) => {
-            this.context.fillText(el, this.textX, this.textY);
-            this.context.strokeText(el, this.textX, this.textY);
+            this.context.fillText(el, this.textX, this.textY + (index * this.lineHeight));
+            this.context.strokeText(el, this.textX, this.textY + (index * this.lineHeight));
          });
-         console.log(linesArray);
+         this.convertToParticales();
       }
       convertToParticales(){
-
+         this.particles = [];
+         const pixels = this.content.getImageData(0, 0, this.canvasWidth, this.canvasHeight).data;
+         this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+         for (let y = 0; y < this.canvasHeight; y += this.gap) {
+            for (let x = 0; x < this.canvasWidth; x += this.gap) {
+               const index = (y * this.canvasWidth + x ) * 4;
+               const alpha = pixels[index + 3];
+               if (alpha > 0){
+                  const red = pixels[index];
+                  const green = pixels[index + 1];
+                  const blue = pixels[index + 2];
+                  const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+                  this.particles.push(new Particle(this, x, y, color));
+               }
+            }
+         }
       }
       render(){
-
+         this.particles.forEach(particle => {
+            particle.update();
+            particle.draw();
+         });
       }
    }
 
    const effect = new Effect(ctx, canvas.width, canvas.height);
    effect.wrapText('Thanks!!! Your message has been sent!');
-   console.log(effect);
+   effect.render();
 
    function animate(){
-
+      ctx.clearRect(0, 0, canvas.width, canvas,height);
+      effect.render();
+      requestAnimationFrame(animate);
    }
+   animate();
+});
 
 
 
+/*
+Vertical Position Adjustment:
+   line 59:  this.verticalOffset = -160;
 
 
+Play With Appearance:
+   change gap property on line 55 and line 16:(- 1).
+   change friction and ease on lines 24 and 25.
+
+Change Origin of Particles:
+      // Modify an ease on y:
+      update(){
+         this.x += (this.originX - this.x) * this.ease;
+         this.y += this.originY - this.y;
+      }
+
+Or: 
+   // This comes from left and right top corners: 
+   class Particle {
+      constructor(effect, x, y, color){
+         this.effect = effect;
+         this.x = Math.random() * this.effect.canvasWidth;
+         this.y = 0;
+
+   // This comes from bottom:
+   this.y = this.effect.canvasHeight;
+
+If you don't use the expensive Math.sqrt, you can just use the formula (line 34), 
+         but use a large radius for you mouse. 
+
+
+Math.atan2() gives an angle in radians between positve x axis and a line projected for 0,0 to particle position.
+
+*/
 
 
 
@@ -141,8 +244,10 @@ window.addEventListener('load', function(){
    }
 
     wrapText('Thanks!!! Your message has been sent!');
-*/
+
 });
+
+*/
 
 
 /////////////////////////////////////////////////////////
